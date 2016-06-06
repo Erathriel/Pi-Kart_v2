e4c2d9fa667d2209a874952adf93d886b278cacc
Erathriel <geoffreynoro@orange.fr>
2016-06-07 00:51:38 +0200

    XML sans espace et avec maj

--- Src/GUI.py
+++ Src/GUI.py
@@ -16,7 +16,7 @@ class mainUI(Frame):
         """Fonction éxécutée à la création de la fenètre, on affiche le canvas, les boutons pour accéder aux différentes boites de dialogue (saisie du polygone, création de
 			la matrice de transformation. On initialise les différentes variables du programme, et on lance la boucle de tkinter."""
         self.master.title('Affichage des figures')
-        self.master.configure(width=1100, height=1000, bg="white")
+        self.master.configure(width=700, height=500, bg="white")
         self.can = Canvas(self.master, height=400, width=400, bg="white", cursor="trek")
         self.can.pack(side=LEFT, anchor="nw")
 
@@ -24,14 +24,14 @@ class mainUI(Frame):
         self.description.pack(side=TOP, anchor="nw")
 
         self.panneautri = Label(self.master, height=10, width=100, bg="white", text="OK")
-        self.panneautri.pack(side=TOP,anchor="nw")
+        self.panneautri.pack(side=TOP, anchor="nw")
 
         self.panneautrif = Label(self.master, height=10, width=100, bg="white", text="OK")
-        self.panneautrif.pack(side=TOP,anchor="nw")
+        self.panneautrif.pack(side=TOP, anchor="nw")
 
         self.v = StringVar()
-        self.choix = Radiobutton(self.master, text=" Rouge ", variable=self.v, value=" Rouge ", bg="white")
-        self.choix2 = Radiobutton(self.master, text=" Bleu ", variable=self.v, value=" Bleu ", bg="white")
+        self.choix = Radiobutton(self.master, text=" Rouge ", variable=self.v, value="Rouge", bg="white")
+        self.choix2 = Radiobutton(self.master, text=" Bleu ", variable=self.v, value="Bleu", bg="white")
         self.choix.pack(side=LEFT, anchor="n")
         self.choix2.pack(side=LEFT, anchor="n")
         self.choix2.select()
@@ -40,14 +40,14 @@ class mainUI(Frame):
         self.triercouleur.pack(side=LEFT, anchor="n")
 
         self.v2 = StringVar()
-        self.choixf1 = Radiobutton(self.master, text=" Circulaire ", variable=self.v2, value=" circulaire ", bg="white")
-        self.choixf2 = Radiobutton(self.master, text=" Triangulaire ", variable=self.v2, value=" triangulaire ", bg="white")
-        self.choixf3 = Radiobutton(self.master, text=" Hexagonal ", variable=self.v2, value=" hexagonal ", bg="white")
+        self.choixf1 = Radiobutton(self.master, text=" Circulaire ", variable=self.v2, value="Circulaire", bg="white")
+        self.choixf2 = Radiobutton(self.master, text=" Triangulaire ", variable=self.v2, value="Triangulaire",
+                                   bg="white")
+        self.choixf3 = Radiobutton(self.master, text=" Hexagonal ", variable=self.v2, value="Hexagonal", bg="white")
         self.choixf1.pack(side=LEFT, anchor="n")
         self.choixf2.pack(side=LEFT, anchor="n")
         self.choixf3.pack(side=LEFT, anchor="n")
         self.choixf1.select()
-
 
         self.trierforme = Button(self.master, text="Tri par forme", command=self.formePanneau, bg="ivory")
         self.trierforme.pack(side=LEFT, anchor="n")
